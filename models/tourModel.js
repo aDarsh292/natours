21e5d57fff640a6021290d3aab9bccb2296bac86
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-01-29 23:21:08 +0530

    Implemented nested routing and factory functions for improved API structure

--- models/tourModel.js
+++ models/tourModel.js
@@ -168,7 +168,7 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query tool ${Date.now() - this.start} milliseconds!`);
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
