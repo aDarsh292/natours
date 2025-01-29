8ad05b88f84d6a304498178bda24b034133afb68
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-01-29 15:10:26 +0530

    Added modeling and populating tour guides in the project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -87,7 +87,7 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
   }
-  
+
   res.status(204).json({
     status: 'success',
     data: null,
