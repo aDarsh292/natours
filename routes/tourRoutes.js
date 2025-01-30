07d300c7149e2890da6327363d471195e44c32f5
Adarsh Kumar <adarshkumar.mnnit@gmail.com>
2025-01-30 21:18:47 +0530

    Add geospatial queries, distance calculation, and duplicate review prevention

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,14 @@ router
     tourController.getMonthlyPlan
   );
 
+// /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi --> standard
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
